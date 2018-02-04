3b394214f6a6c05ce5a50d0f0bba947945aa46c8
Jonathan Byrd <falcoslyviper@gmail.com>
2018-02-04 14:30:27 -0500

    upgrade nuget packages

--- BudgetMagic/Models/CardDataDBContext.cs
+++ BudgetMagic/Models/CardDataDBContext.cs
@@ -14,7 +14,6 @@ namespace BudgetMagic.Models
         public DbSet<MagicSet> Sets { get; set; }
         public DbSet<MagicBlock> Blocks { get; set; }
         public DbSet<Format> Formats { get; set; }
-        public DbSet<Rules> Rules { get; set; }
         public DbSet<Currency> Currencies { get; set; }
     }
 }
--- BudgetMagic/Models/Format.cs
+++ BudgetMagic/Models/Format.cs
@@ -10,6 +10,20 @@ namespace BudgetMagic.Models
         public int FormatID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public Rules Rules { get; set; }
+        public int MainboardCardLimit { get; set; }
+        public int MainboardCardMinimum { get; set; }
+        public int SideboardCardLimit { get; set; }
+        public bool HasCommanders { get; set; }
+        public bool IsSignleton { get; set; }
+        public int MinCMC { get; set; }
+        public int MaxCMC { get; set; }
+        public int MaxUncommonCards { get; set; }
+        public int MaxRareCards { get; set; }
+        public int MaxMythicCards { get; set; }
+        public virtual Currency Currency { get; set; }
+        public double MaxCardPrice { get; set; }
+        public double MaxCommanderPrice { get; set; }
+        public virtual ICollection<MagicSet> SetRestriction { get; set; }
+        public virtual ICollection<MagicBlock> BlockRestrictions { get; set; }
     }
 }
